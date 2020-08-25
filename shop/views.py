354010f158e233e2faab80e822a45ca1573ed344
RolandMcDoland <miki.franek@gmail.com>
2020-08-25 23:36:41 +0200

    Init main template

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+offers = [
+    {
+        'developer': 'CD Projekt RED', 
+        'name': 'The Witcher 3: Wild Hunt',
+        'description': 'Greatest game of all time',
+        'release_date': 'May 18, 2015'
+    },
+    {
+        'developer': 'Arkane Studios', 
+        'name': 'Dishonored 2',
+        'description': 'Most creative game of all time',
+        'release_date': 'November 11, 2016'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>sf</h1>')
+    context = { 'offers': offers }
+    return render(request, 'shop/home.html', context)
