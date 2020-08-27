20c119b662ecf034e6f8c833237f38ea459a43ea
RolandMcDoland <miki.franek@gmail.com>
2020-08-27 18:36:22 +0200

    Database offer model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Offer(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    release_date = models.DateField()
+    developer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
-
-offers = [
-    {
-        'developer': 'CD Projekt RED', 
-        'name': 'The Witcher 3: Wild Hunt',
-        'description': 'Greatest game of all time',
-        'release_date': 'May 18, 2015'
-    },
-    {
-        'developer': 'Arkane Studios', 
-        'name': 'Dishonored 2',
-        'description': 'Most creative game of all time',
-        'release_date': 'November 11, 2016'
-    }
-]
+from .models import Offer
 
 def home(request):
-    context = { 'offers': offers }
+    context = { 'offers': Offer.objects.all() }
     return render(request, 'shop/home.html', context)
