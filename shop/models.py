f548a6318a0bf6e4c34b86a9e8a22a66d2d4246c
RolandMcDoland <miki.franek@gmail.com>
2020-09-04 13:08:30 +0200

    Cover images added

--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,8 @@ class Offer(models.Model):
     description = models.TextField()
     release_date = models.DateField()
     developer = models.CharField(max_length=100)
+    in_order = models.BooleanField(default=False)
+    cover_image = models.CharField(max_length=500, default="https://store-images.s-microsoft.com/image/apps.28990.69531514236615003.8f0d03d6-6311-4c21-a151-834503c2901a.d629260e-2bc4-4588-950c-f278cbc22a64")
 
     def __str__(self):
         return self.name
