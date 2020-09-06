16c6d73b4d588e8d44847957008333899d8f755d
RolandMcDoland <miki.franek@gmail.com>
2020-09-06 14:46:14 +0200

    Payment with PayU integration

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('cart', views.cart, name='shop-cart'),
     path('cart/add/<str:name>/', views.add_to_cart),
     path('cart/remove/<str:name>/', views.remove_from_cart),
+    path('cart/order/', views.order, name='shop-order'),
+    path('cart/order/payment', views.payment, name='shop-payment'),
+    path('cart/order/thanks', views.thanks, name='shop-thanks'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Offer
+from .authorization import auth_dict
+import requests
+import json
 
 def home(request):
     context = { 'offers': Offer.objects.all() }
@@ -26,3 +29,53 @@ def remove_from_cart(request, name):
     offer_to_update.in_order = False
     offer_to_update.save()
     return redirect('shop-cart')
+
+def order(request):
+    return render(request, 'shop/order.html')
+
+def payment(request):
+    auth_response = requests.post("https://secure.snd.payu.com/pl/standard/user/oauth/authorize", data=auth_dict)
+    auth_cred = json.loads(auth_response.text)
+
+    items = Offer.objects.filter(in_order = True)
+
+    sum = 0
+    products = []
+
+    for item in items:
+        sum += item.price
+        product = { 'name': item.name, 'unitPrice': str(int(item.price * 100)), 'quantity': '1' }
+        products.append(product)
+
+        item.in_order = False
+        item.save()
+
+    payment_dict = {
+        "notifyUrl": "http://127.0.0.1:8000/",
+        "customerIp": "127.0.0.1",
+        "merchantPosId": "394143",
+        "description": "Games shop",
+        "currencyCode": "PLN",
+        "totalAmount": str(int(sum * 100)),
+        "continueUrl": "http://127.0.0.1:8000/cart/order/thanks",
+        "buyer": {
+            "email": request.POST.get("email"),
+            "phone": request.POST.get("phone"),
+            "firstName": request.POST.get("firstName"),
+            "lastName": request.POST.get("lastName"),
+            "language": "pl"
+        },
+        "settings":{
+            "invoiceDisabled":"true"
+        },
+        "products": products
+    }
+
+    headers = {'Content-type': 'application/json', 'Authorization': 'Bearer ' + auth_cred['access_token']}
+
+    payU_response = requests.post("https://secure.snd.payu.com/api/v2_1/orders", json=payment_dict, headers=headers, allow_redirects=False)
+
+    return redirect(json.loads(payU_response.text)['redirectUri'])
+
+def thanks(request):
+    return render(request, 'shop/thanks.html')
